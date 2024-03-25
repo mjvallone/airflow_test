4bedd15333c9a42ec737cba47213a48a63690c11
Martin Vallone <mjvallone@gmail.com>
2024-03-25 15:56:38 -0300

    se agrega IPO

--- dags/get_final_alumnos_programas.py
+++ dags/get_final_alumnos_programas.py
@@ -30,7 +30,6 @@ query_alumnos_programas = """
     AND mpc.ano = pm."N_ANO_CARRERA" 
     AND mpc.semestre = CASE WHEN CAST(MOD(CAST(an.f_rinde AS date) - CAST(ap.fecha_inicio AS date),365) as decimal)/365 > 0.5 THEN 2 ELSE 1 END
     WHERE ap."F_GRADUACION" IS NULL AND pm."N_ANO_CARRERA" <= ((current_date - CAST(ap.fecha_inicio AS date))/365)+1 --limitamos la cantidad de registros para que sea mas liviano
-    AND ap."N_ID_PERSONA" in (229632, 170434, 200572, 175447)
     AND an.m_aprueba_mat ='Si'
     ORDER BY pm."N_ANO_CARRERA", semestre
 """
@@ -64,14 +63,17 @@ def process_data():
         # reviso cuales de las materias de la carrera previa, deben ser consideradas para la carrera actual
         for index, row in subjects_to_check.iterrows():
             persona_row = current_degree_for_people_with_cc[current_degree_for_people_with_cc["N_ID_PERSONA"] == row["N_ID_PERSONA"]]
-            current_data = persona_row[["C_IDENTIFICACION", "C_PROGRAMA", "C_ORIENTACION", "c_plan"]]
-            subject_id_to_check = row["N_ID_MATERIA"]
+            if not persona_row.empty:
+                current_data = persona_row[["C_IDENTIFICACION", "C_PROGRAMA", "C_ORIENTACION", "c_plan"]]
+                subject_id_to_check = row["N_ID_MATERIA"]
 
-            key_to_find = int(f'{int(current_data["C_IDENTIFICACION"].values[0])}{int(current_data["C_PROGRAMA"].values[0])}{int(current_data["C_ORIENTACION"].values[0])}{int(current_data["c_plan"].values[0])}{int(subject_id_to_check)}')
+                key_to_find = int(f'{int(current_data["C_IDENTIFICACION"].values[0])}{int(current_data["C_PROGRAMA"].values[0])}{int(current_data["C_ORIENTACION"].values[0])}{int(current_data["c_plan"].values[0])}{int(subject_id_to_check)}')
 
-            if (planes_materias[planes_materias["unique_key"] == key_to_find]).empty == False:
-                # busco la materia de la carrera anterior en planes_materias de la carrera actual
-                 subjects_to_check.at[index, "found_in_plans_materias"] = True
+                if (planes_materias[planes_materias["unique_key"] == key_to_find]).empty == False:
+                    # busco la materia de la carrera anterior en planes_materias de la carrera actual
+                    subjects_to_check.at[index, "found_in_plans_materias"] = True
+            else:
+                 print("No se encontraron materias para la persona: ", row["N_ID_PERSONA"])
 
         # filtro las materias que estan en la carrera actual de la persona
         valid_subjects = subjects_to_check[subjects_to_check["found_in_plans_materias"] == True]
@@ -79,12 +81,17 @@ def process_data():
         # quito de alumnos_programa las rows con las materias que no corresponden o son de una carrera dada de baja
         filtered_alumnos_programa = alumnos_programa[((alumnos_programa["C_BAJA"] == "CC") & (alumnos_programa["N_ID_MATERIA"].isin(valid_subjects["N_ID_MATERIA"]))) | (alumnos_programa["F_BAJA"].isna())]
 
-        #FIXME si filtered_alumnos_programa tiene alumnos_programa["C_BAJA"] == "CC" , todos los rows de la persona deberian tener el N_ID_ALU_PROG mas alto
+        # para las personas que cambiaron de carrera (["C_BAJA"] == "CC"), se debe considerar el N_ID_ALU_PROG mas alto (pertence a la carrera actual)
+        current_id_alu_prog_for_cc = filtered_alumnos_programa.groupby(["N_ID_PERSONA"])["N_ID_ALU_PROG"].max().reset_index(name='CURRENT_N_ID_ALU_PROG')
+        merged_df = filtered_alumnos_programa.merge(current_id_alu_prog_for_cc, on='N_ID_PERSONA', how='left')
+        merged_df.loc[merged_df["C_BAJA"] == "CC", 'N_ID_ALU_PROG'] = merged_df.loc[merged_df["C_BAJA"] == "CC", 'CURRENT_N_ID_ALU_PROG']
+        merged_df.drop(columns=['CURRENT_N_ID_ALU_PROG'], inplace=True)
+        filtered_alumnos_programa = merged_df
 
         # elimino el campo c_baja y f_baja
         filtered_alumnos_programa = filtered_alumnos_programa.drop(columns=["C_BAJA", "F_BAJA"])
 
-        # falta la suma de las materias aprobadas
+        # calculamos falta la suma de las materias aprobadas por alumno con detalle por ano y semestre
         grouped_alumnos_programa = filtered_alumnos_programa.groupby([
             "N_ID_PERSONA",
             "N_ID_ALU_PROG",
@@ -96,14 +103,24 @@ def process_data():
         ]).size().reset_index(name='cant_mat_aprobadas')        
         grouped_alumnos_programa["estado"] = grouped_alumnos_programa["cant_mat_aprobadas"] - grouped_alumnos_programa["cant_mat_requeridas"]
 
+        # calculamos falta la suma de las materias aprobadas por alumno (si una persona cursa 2 carreras, apareceran 2 rows)
         grouped_by_alumnos = grouped_alumnos_programa.groupby([
-             "N_ID_PERSONA",
+            "N_ID_PERSONA",
             "N_ID_ALU_PROG",
         ]).agg(
              tot_cant_mat_req=('cant_mat_requeridas', 'sum'),
              tot_cant_mat_apr=('cant_mat_aprobadas', 'sum')
         ).reset_index()
         grouped_by_alumnos["estado"] = grouped_by_alumnos["tot_cant_mat_apr"] - grouped_by_alumnos["tot_cant_mat_req"]
+
+        # agregamos IPO a ambos DFs
+        current_values = filtered_alumnos_programa.groupby(["N_ID_PERSONA", "N_ID_ALU_PROG"]).last().reset_index()
+        grouped_alumnos_programa = grouped_alumnos_programa.merge(current_values[["N_ID_PERSONA", "N_ID_ALU_PROG", "C_IDENTIFICACION", "C_PROGRAMA", "C_ORIENTACION"]],
+                                            on=["N_ID_PERSONA", "N_ID_ALU_PROG"], how="left")
+
+        current_values = filtered_alumnos_programa.groupby(["N_ID_PERSONA", "N_ID_ALU_PROG"]).last().reset_index()
+        grouped_by_alumnos = grouped_by_alumnos.merge(current_values[["N_ID_PERSONA", "N_ID_ALU_PROG", "C_IDENTIFICACION", "C_PROGRAMA", "C_ORIENTACION"]],
+                                            on=["N_ID_PERSONA", "N_ID_ALU_PROG"], how="left")
 
         grouped_alumnos_programa.to_csv("grouped_alumnos_programa.csv", index=False)
         grouped_by_alumnos.to_csv("grouped_by_alumnos.csv", index=False)
